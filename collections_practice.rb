be4262e6a7dd0d4ae0a71271fce216b029e61087
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-04-03 18:20:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
-# your code goes here
+require 'pry'
+
+
+def begins_with_r(tools_array)
+  tools_array.all? {|tool| tool.to_s.start_with?("r")}
+end
+  
+
+def contain_a(array)
+  array.select {|word| word.to_s.include?("a")}
+end
+  
+  
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
+end
+  
+  
+def remove_non_strings(array)
+end
+  
+  
+  
